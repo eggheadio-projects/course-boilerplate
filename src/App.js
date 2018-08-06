6ee905057a57cb7b0d2de35b3e1983da6d4e1e50
Rory Smith <rory.smith@korelogic.co.uk>
2018-08-06 01:36:13 +0100

    Change example

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-import TransitionAReactComponentWithCSSTransition from './transition-a-react-component-with-rtgs-csstransition';
+// import TransitionAReactComponentWithCSSTransition from './transition-a-react-component-with-rtgs-csstransition';
 // import Appear from './show-rtg-elements-with-an-initial-transition-with-the-appear-prop';
 // import EnterExit from './disable-rtg-enter-andor-exit-transitions-using-the-enter-and-exit-props';
 // import LifecycleProps from './fine-tune-transitions-with-react-transition-groups-lifecycle-props';
 // import TransitionListWithReactTransitionGroup from './transition-items-within-a-list-with-react-transition-group';
 // import ComponentProp from './choose-which-kind-of-element-renders-as-the-lesstransitiongreater-lesscsstransitiongreater-and-lesstransitiongroupgreater-container-element-with-the-component-prop';
-// import ReactRouterTransition from './transition-between-pages-with-react-transition-group-and-react-router';
+import ReactRouterTransition from './transition-between-pages-with-react-transition-group-and-react-router';
 // import TransitionAReactComponentWithTransition from './transition-a-react-component-with-rtgs-transition';
 // import StyledComponentsExample from './use-react-transition-group-alongside-the-styled-components-library';
 // import MigrateFromV1 from './migrate-to-using-react-transition-group-v2-from-v1';
@@ -14,13 +14,13 @@ import TransitionAReactComponentWithCSSTransition from './transition-a-react-com
 class App extends Component {
   render() {
     return (
-      <TransitionAReactComponentWithCSSTransition />
+      // <TransitionAReactComponentWithCSSTransition />
       // <Appear />
       // <EnterExit />
       // <LifecycleProps />
       // <TransitionListWithReactTransitionGroup />
       // <ComponentProp />
-      // <ReactRouterTransition />
+      <ReactRouterTransition />
       // <TransitionAReactComponentWithTransition />
       // <StyledComponentsExample />
       // <MigrateFromV1 />
