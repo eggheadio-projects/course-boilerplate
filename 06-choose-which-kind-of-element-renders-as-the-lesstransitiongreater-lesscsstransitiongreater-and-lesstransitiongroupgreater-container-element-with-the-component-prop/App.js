e001eb477fba4b80f2809fab98d8dc94ef9872b1
Zac Jones <zacjones93@gmail.com>
2019-01-15 22:52:16 -0500

    missed import

--- 06-choose-which-kind-of-element-renders-as-the-lesstransitiongreater-lesscsstransitiongreater-and-lesstransitiongroupgreater-container-element-with-the-component-prop/App.js
+++ 06-choose-which-kind-of-element-renders-as-the-lesstransitiongreater-lesscsstransitiongreater-and-lesstransitiongroupgreater-container-element-with-the-component-prop/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import ComponentProp from './06-choose-which-kind-of-element-renders-as-the-lesstransitiongreater-lesscsstransitiongreater-and-lesstransitiongroupgreater-container-element-with-the-component-prop';
+import ComponentProp from './lesson';
 
 class App extends Component {
   render() {
